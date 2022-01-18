2b12d7bccd378c32c58262c9f770719e4ff1d565
Aurel1971 <augustindinu@gmail.com>
2022-01-18 21:02:46 +0200

    curs 1

---
+++ hello/hello.js
@@ -0,0 +1,17 @@
+
+
+// ruleaza tellMe dupa 3000 de ms (3secunde):
+setTimeout(tellMe, 3000)
+
+//creaza o noua instructiune (functie) numita tellMe
+function tellMe() {
+    alert("Hello world!")
+    alert("Welcome to JavaScript!")
+}
+     
+
+/*
+comentariu pe 
+mai multe linii */
+
+// comentariu pentru o singura linie
---
+++ hello/variabile/variabile.js
@@ -0,0 +1,16 @@
+
+// Punem data curenta in variabila date
+var date = new Date()
+
+
+var hour = date.getHours()
+var minutes = date.getMinutes()
+var isRaining = false
+
+// aam adaugat la un sir o variabila (concatenare)
+var message = "Hello! Este ora " + hour + ":" + minutes
+
+// o variabila fara valoare (undefined)
+var city
+
+alert(message)
